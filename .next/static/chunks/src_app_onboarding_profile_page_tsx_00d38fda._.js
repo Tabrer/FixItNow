9efc0c410cfc4ab2cf290ed625d68d66ac177f3e
Tabrer <khowajaunknown2002@gmail.com>
2025-08-07 14:29:24 +0500

    Delete .next directory

--- .next/static/chunks/src_app_onboarding_profile_page_tsx_00d38fda._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_194346d3._.js",
-  "static/chunks/src_f8d34c07._.js"
-],
-    source: "dynamic"
-});
